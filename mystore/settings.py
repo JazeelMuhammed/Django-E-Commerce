28960015f41af0993872d73d30efd335622aef4c
JazeelMuhammed <126056206+JazeelMuhammed@users.noreply.github.com>
2023-08-17 14:10:32 +0530

    Updated db-config

--- mystore/settings.py
+++ mystore/settings.py
@@ -100,17 +100,33 @@ AUTH_PROFILE_MODULE = "accounts.UserProfile"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        # 'ENGINE': 'django.db.backends.sqlite3',
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'test',
-        'USER': 'postgres',
-        'PASSWORD': 'nabeesa',
-        'HOST': 'localhost',
-        'PORT': '5432',
+if DEBUG and (DATABASE == 1):
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
+
+elif DEBUG and (DATABASE == 2):
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'test',
+            'USER': 'postgres',
+            'PASSWORD': 'nabeesa',
+            'HOST': 'localhost',
+            'PORT': '5432',
+        }
+    }
+
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
